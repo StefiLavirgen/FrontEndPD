fc3daf7e9b3044d4b24658168964d7554f2fe71b
Stefania Rocio Lavirgen Ercoli <113640705+StefiLavirgen@users.noreply.github.com>
2022-11-11 15:09:21 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+alert ("Preparado para cocinar?")
+
+function cambiarfondo() { document.body.style.backgroundColor= "darkblue";
+document.body.style.color= "white";
+document.getElementById("link").style.color= "white";
+document.getElementById("link1").style.color= "white";
+document.getElementById("link2").style.color= "white";
+document.getElementById("link3").style.color= "white";
+document.getElementById("titulo").style.color= "white";
+document.getElementById("recetas").style.color= "white";
+document.getElementById("map").style.color= "white";
+}
+
+function copyright() {
+    alert ("Datos de autor: Stefania Lavirgen, stefi-lavi@hotmail.com")
+}
