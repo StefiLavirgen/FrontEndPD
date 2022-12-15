ded69481bf0846fd2afb69e112543f49d8d2238a
Stefania Rocio Lavirgen Ercoli <113640705+StefiLavirgen@users.noreply.github.com>
2022-12-15 17:41:05 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,89 @@
-alert ("Preparado para cocinar?")
-
-function cambiarfondo() { document.body.style.backgroundColor= "darkblue";
-document.body.style.color= "white";
-document.getElementById("link").style.color= "white";
-document.getElementById("link1").style.color= "white";
-document.getElementById("link2").style.color= "white";
-document.getElementById("link3").style.color= "white";
-document.getElementById("titulo").style.color= "white";
-document.getElementById("recetas").style.color= "white";
-document.getElementById("map").style.color= "white";
+function zoomIn()
+{
+    imgZoom1.style.height = "100%";
+    imgZoom1.style.width = "100%";
 }
 
-function copyright() {
-    alert ("Datos de autor: Stefania Lavirgen, stefi-lavi@hotmail.com")
+function zoomOut(){
+    imgZoom1.style.height = "";
+    imgZoom1.style.width = "";
+}
+
+function zoomIn2()
+{
+    imgZoom2.style.height = "100%";
+    imgZoom2.style.width = "100%";
+}
+
+function zoomOut2(){
+    imgZoom2.style.height = "";
+    imgZoom2.style.width = "";
+}
+
+function zoomIn3()
+{
+    imgZoom3.style.height = "100%";
+    imgZoom3.style.width = "100%";
+}
+
+function zoomOut3(){
+    imgZoom3.style.height = "";
+    imgZoom3.style.width = "";
+}
+
+function zoomIn4()
+{
+    imgZoom4.style.height = "100%";
+    imgZoom4.style.width = "100%";
+}
+
+function zoomOut4(){
+    imgZoom4.style.height = "";
+    imgZoom4.style.width = "";
+}
+
+/*abajo*/
+
+function zoomInAbajo5()
+{
+    imgZoom5.style.height = "100%";
+    imgZoom5.style.width = "100%";
+}
+
+function zoomOutAbajo5(){
+    imgZoom5.style.height = "";
+    imgZoom5.style.width = "";
+}
+
+function zoomInAbajo6()
+{
+    imgZoom6.style.height = "100%";
+    imgZoom6.style.width = "100%";
+}
+
+function zoomOutAbajo6(){
+    imgZoom6.style.height = "";
+    imgZoom6.style.width = "";
+}
+
+function zoomInAbajo7()
+{
+    imgZoom7.style.height = "100%";
+    imgZoom7.style.width = "100%";
+}
+
+function zoomOutAbajo7(){
+    imgZoom7.style.height = "";
+    imgZoom7.style.width = "";
+}
+
+function zoomInAbajo8()
+{
+    imgZoom8.style.height = "100%";
+    imgZoom8.style.width = "100%";
+}
+
+function zoomOutAbajo8(){
+    imgZoom8.style.height = "";
+    imgZoom8.style.width = "";
 }
